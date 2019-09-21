46464a7d23f45a736123fbb0f100fc42cce78494
astriskit <harry2office@gmail.com>
2019-09-22 00:20:33 +0530

    build v1.0-alpha

---
+++ doc/precache-manifest.2fb7da0bc87cfbdb30d3e543c022d41f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "560d7bea120c98b0d846d4d96c9bd4d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f6646932d5308f72cd2",
+    "url": "/static/css/main.c7ef46d8.chunk.css"
+  },
+  {
+    "revision": "046ecd15163c92a302e2",
+    "url": "/static/js/2.5ec49af3.chunk.js"
+  },
+  {
+    "revision": "9f6646932d5308f72cd2",
+    "url": "/static/js/main.1c6b279c.chunk.js"
+  },
+  {
+    "revision": "30628294aed638c24e8f",
+    "url": "/static/js/runtime~main.03b948bd.js"
+  }
+]);

e30e244adeb87d7b0f6bab9f3e7e4a42ac192445
Shrey Naygandhi <shrey.naygandhi@innoplexus.com>
2019-10-13 20:59:32 +0530

    Fix Regex for special chars in URL

--- src/utils.js
+++ src/utils.js
@@ -8,7 +8,7 @@ export const  isvalidYoutubeUrl = (getURL) => {
 	
 	if(host.search(/youtube\.com|youtu\.be/)===-1) return false;
 	if(host.search(/youtu\.be/)!==-1) return path.slice(1);
-	if(path.search(/embed/)!==-1) return /embed\/([A-z0-9]+)(&|$)/.exec(path)[1];
-	var getId = /v=([\w\-\_]*)(&|$)/.exec(srch);
+	if(path.search(/embed/)!==-1) return /embed\/([A-z0-9]+)(\&|$)/.exec(path)[1];
+	var getId = /v=([a-zA-Z0-9_\-]*)(\&|$)/.exec(srch);
 	if(host.search(/youtube\.com/)!==-1) return !getId ? '':getId[1];
 }

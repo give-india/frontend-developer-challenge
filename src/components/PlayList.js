97d213c9bb444cea70f36bda23311cfeb40125c1
Shrey Naygandhi <shrey.naygandhi@innoplexus.com>
2019-10-13 16:03:17 +0530

    Add Design for PlayList

--- src/components/PlayList.js
+++ src/components/PlayList.js
@@ -2,17 +2,28 @@ import React from 'react';
 import PropTypes from "prop-types";
 
 const PlayList = (props) => (
-    <div className="play-list">
+    <div className="playlist">
         <div className="playlist-header">
             Playlist
-            </div>
+        </div>
+        <div className="playlist-list">
         {
             props.list.map((video,index) => {
                 return (
-                    <div className="play-list-card" key={index}>{video.title}</div>
+                    <div className="playlist-card" key={index}>
+                        <img className="thumb" src={video.thumbnail_url} />
+                        <div className='title'>{video.title}
+                        <a href={video.author_url} target="_blank" rel="noopener noreferrer">{video.author_name}</a>
+                        </div>
+                        <span className="remove">x</span>
+                    </div>
                 )
             })
         }
+            
+        </div>
+
+        
 
 
     </div>

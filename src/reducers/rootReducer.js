d7dab8c8955cfcc8d8d670b1b66e7888cb310baf
Pramod <npramod95@gmail.com>
2019-11-30 15:05:03 +0530

    maintain state accross all tabs

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,9 @@
 import { combineReducers } from "redux";
+import { withReduxStateSync } from 'redux-state-sync'
 import PlaySongsReducer from "./PlaySongsReducer.js";
 
 const rootReducer = combineReducers({
   songsData: PlaySongsReducer
 });
 
-export default rootReducer;
+export default withReduxStateSync(rootReducer)
--- src/stores/store.js
+++ src/stores/store.js
@@ -1,13 +1,27 @@
 import { applyMiddleware, createStore } from "redux";
+import { createStateSyncMiddleware, initStateWithPrevTab } from 'redux-state-sync';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/rootReducer';
 import axios from 'axios';
 
 axios.defaults.withCredentials = true;
-const middleware = applyMiddleware(thunk.withExtraArgument(axios));
+
+const config = {
+    // TOGGLE_TODO will not be triggered in other tabs
+    blacklist: ['TOGGLE_TODO'],
+  }
+
+const middlewares = [
+    createStateSyncMiddleware(config),
+    thunk.withExtraArgument(axios)
+  ];
+
+const middleware = applyMiddleware(...middlewares);
 
 
 const store = createStore(rootReducer, composeWithDevTools(middleware));
 
+// init state with other tabs
+initStateWithPrevTab(store)
 export default store;

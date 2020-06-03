4882ae29af187f6c0ae4b1d3a93bbebfc7781776
Priyanka-ashok <priyanka.ashok@quintype.com>
2020-06-03 21:00:58 +0530

    Feat: Simple video player app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import VideoPlayer from "./components/videoPlayer";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      Radio App
+      <VideoPlayer />
     </div>
   );
 }

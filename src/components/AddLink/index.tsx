45f62151afdd36119397519ce5cf6adab9b4a8d0
Shorya Kaushik <shorya.kaushik@hashedin.com>
2020-06-19 18:50:18 +0530

    Add components
    *Add Link component
    *Add Queue component

---
+++ src/components/AddLink/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./style.css";
+
+export const AddLink: React.FC = () => {
+  return (
+    <div className="link-main">
+         
+    </div>
+  );
+}
+
---
+++ src/components/PlayAndQueue/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./style.css";
+
+export const PlayAndQueue: React.FC = () => {
+  return (
+    <div className="playqueue-main">
+         
+    </div>
+  );
+}
+

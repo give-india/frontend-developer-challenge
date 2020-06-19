8e483544f96901f1fecb190286e2d39e61924aa5
Shorya Kaushik <shorya.kaushik@hashedin.com>
2020-06-19 19:56:24 +0530

    Modify Playqueue Component
    *Component separated into two divisions

--- src/components/PlayAndQueue/index.tsx
+++ src/components/PlayAndQueue/index.tsx
@@ -4,7 +4,12 @@ import "./style.css";
 export const PlayAndQueue: React.FC = () => {
   return (
     <div className="playqueue-main">
-         
+         <div className = "play-video">
+              <h1>play screen</h1>
+         </div>
+         <div className = "play-queue">
+              <h1>play queue</h1>
+         </div>
     </div>
   );
 }

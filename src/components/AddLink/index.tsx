b13e998f37efa2f9efd99fdb2dab72a6c415cd7f
Shorya Kaushik <shorya.kaushik@hashedin.com>
2020-06-19 19:48:53 +0530

    Modify AddLink component
    *Add textfield
    *Add button

--- src/components/AddLink/index.tsx
+++ src/components/AddLink/index.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
 import "./style.css";
+import "antd/dist/antd.css";
+
+import { Input,Button } from "antd";
 
 export const AddLink: React.FC = () => {
   return (
     <div className="link-main">
-         
+      <div>
+        <Input className = "link-input" size="large" placeholder="Enter Link" />
+      </div>
+      <div className="add-link-button">
+      <Button type="primary" size="large">
+          Add Link
+        </Button>
+      </div>
     </div>
   );
-}
-
+};

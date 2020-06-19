2166f8c45312e6a7b7fb491e8ee852545bff518d
Shorya Kaushik <shorya.kaushik@hashedin.com>
2020-06-19 20:29:32 +0530

    Add links in App
    *Add link entry object

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,49 @@
-import React from 'react';
+import React from "react";
 import "./App.css";
 
-import { AddLink } from './components/AddLink'
-import { PlayAndQueue } from './components/PlayAndQueue'
+import { AddLink } from "./components/AddLink";
+import { PlayAndQueue } from "./components/PlayAndQueue";
+import { storageKey } from "./constants/constants";
+
+export interface LinkEntry {
+  name: string;
+  id: number;
+}
 
 const App: React.FC = () => {
+
+  
+  const onAddEntry = (entry: LinkEntry) => {
+    if (entries) {
+        const newTasks = [...entries, entry];
+        storeTaskEntries(newTasks);
+    } else {
+        storeTaskEntries([entry]);
+    }
+};
+
+  const loadTaskEntries = () => {
+    const entriesString = window.localStorage.getItem(storageKey);
+    const entries = entriesString ? JSON.parse(entriesString) : [];
+    return entries;
+  };
+
+  const [entries, setEntries] = React.useState<LinkEntry[]>(loadTaskEntries());
+  const storeTaskEntries = (entries: LinkEntry[]) => {
+    window.localStorage.setItem(storageKey, JSON.stringify(entries));
+    setEntries(loadTaskEntries());
+  };
+
   return (
     <div className="App">
-         <div className = "link">
-              <AddLink />
-         </div>
-         <div className = "queue">
-               <PlayAndQueue />
-         </div>
+      <div className="link">
+        <AddLink onChange = {onAddEntry}/>
+      </div>
+      <div className="queue">
+        <PlayAndQueue />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/AddLink/index.tsx
+++ src/components/AddLink/index.tsx
@@ -2,16 +2,42 @@ import React from "react";
 import "./style.css";
 import "antd/dist/antd.css";
 
-import { Input,Button } from "antd";
+import { Input, Button } from "antd";
+import { LinkEntry } from "../../App";
 
-export const AddLink: React.FC = () => {
+interface AddLinkProps {
+
+  onChange: (x: LinkEntry) => void;
+  
+}
+
+
+export const AddLink: React.FC<AddLinkProps> = (props: AddLinkProps) => {
+  const [link, setLink] = React.useState("");
+
+  const onLinkChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setLink(event.target.value);
+  };
+
+  const onAddEntry = () => {
+    const entry: LinkEntry = { name, id: Date.now() };
+    props.onChange(entry);
+  };
+
+
   return (
     <div className="link-main">
       <div>
-        <Input className = "link-input" size="large" placeholder="Enter Link" />
+        <Input
+          className="link-input"
+          size="large"
+          placeholder="Enter Link"
+          onChange={onLinkChange}
+          value={link}
+        />
       </div>
       <div className="add-link-button">
-      <Button type="primary" size="large">
+        <Button type="primary" size="large" onClick={onAddEntry}>
           Add Link
         </Button>
       </div>

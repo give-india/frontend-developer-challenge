b284ec2de4e34cfa376144b2726cab6bebe9e956
Shorya Kaushik <shorya.kaushik@hashedin.com>
2020-06-20 19:38:05 +0530

    Add Youtube link Validation *Use Regex to validate youtube url

--- src/components/AddLink/index.tsx
+++ src/components/AddLink/index.tsx
@@ -2,15 +2,12 @@ import React from "react";
 import "./style.css";
 import "antd/dist/antd.css";
 
-import { Input, Button } from "antd";
+import { Input, Button, Modal } from "antd";
 import { LinkEntry } from "../../App";
 
 interface AddLinkProps {
-
   onChange: (x: LinkEntry) => void;
-
 }
-
 
 export const AddLink: React.FC<AddLinkProps> = (props: AddLinkProps) => {
   const [link, setLink] = React.useState("");
@@ -20,10 +17,26 @@ export const AddLink: React.FC<AddLinkProps> = (props: AddLinkProps) => {
   };
 
   const onAddEntry = () => {
-    const entry: LinkEntry = { link, id: Date.now() };
-    props.onChange(entry);
-  };
+    let validate = false;
+    let regexp: RegExp = /^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})?$/;
+    
 
+    
+    if(regexp.test(link))
+    {
+      validate = true;
+    }
+    if (validate) {
+      const entry: LinkEntry = { link, id: Date.now() };
+      props.onChange(entry);
+    } else {
+      Modal.warning({
+        title: "Invalid URL",
+        content: "Please enter URL in proper format",
+      });
+      
+    }
+  };
 
   return (
     <div className="link-main">

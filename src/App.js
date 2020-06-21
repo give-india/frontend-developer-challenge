ecdd5a2d59fb478322fc3531cfb6946b630f7fa8
AYUSH SINHA <ayush.sinha@neudesic.com>
2020-06-21 22:26:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./App.css";
+
+import Videoplayer from "../src/videoplayer/videoplayer";
+
+function App() {
+  return (
+    <div>
+      <Videoplayer />
+    </div>
+  );
+}
+
+export default App;

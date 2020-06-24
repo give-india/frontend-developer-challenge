f84f5bfe4fe8f8a87ede28632523aceba51103bb
Srigadgeter <sridhar.t@ezedox.com>
2020-06-24 15:52:36 +0530

    playing item indicator has been added

--- src/components/Playlist/LinkCard/LinkCard.js
+++ src/components/Playlist/LinkCard/LinkCard.js
@@ -10,10 +10,13 @@ const LinkCard = ({
 }) => (
         <div className={`linkCard ${cardId} ${cardIsActive ? 'activeCard' : ''}`}>
             <span className="cardContext">{cardContext}</span>
-            <span 
-                className="removeBtn"
-                onClick={removeCardHandler}
-            >x</span>
+            <div>
+                <span className="playingIndicator">{cardIsActive ? 'Playing' : ''}</span>&nbsp;
+                <span
+                    className="removeBtn"
+                    onClick={removeCardHandler}
+                >x</span>
+            </div>
         </div>
     )
 

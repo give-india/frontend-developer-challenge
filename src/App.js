338156639d99e6bd5fde489b0c5b1a17c3bbd522
Akanksha80 <84960940+Akanksha80@users.noreply.github.com>
2022-01-15 17:15:36 +0530

    Added src folder

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import VideoPlayer from "./components/videoPlayer";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <VideoPlayer />
+    </div>
+  );
+}
+
+export default App;
